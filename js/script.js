67f3bf4b42a39d9130a01506f1aa750c2de864a9
hummm <xoooxoooxx@gmail.com>
2014-06-04 21:52:47 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -74,7 +74,7 @@
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
-      document.getElementById('status').innerHTML =
+      document.getElementById('main').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
   }
