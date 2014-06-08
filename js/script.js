9eb2be3fb26f29c6bd3ac924dc8d885c601540f8
hummm <xoooxoooxx@gmail.com>
2014-06-08 17:52:34 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,7 +23,13 @@ FB.getLoginStatus(function(response) {
   }
  });
 
-
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
 //以下為canvas的程式碼，基本上不需多動，依據comments修改即可
 	
 	//起始畫面
